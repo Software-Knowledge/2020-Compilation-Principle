b4ce9199c94f34bfcbf37551c28256ad1ca0cd9b
stormbroken <181250184@smail.nju.edu.cn>
2020-11-17 14:54:03 +0800

    2020-Compilation-Principle update at 11/17/2020 Tue

--- upload.bat
+++ upload.bat
@@ -1,9 +1,7 @@
 @echo off
-set date=%DATE%
-set time=%TIME%
 for /f "delims=" %%i in ("%cd%") do set folder=%%~ni
 
 git add .
 git commit -m "%folder% update at %date%"
 git push
-echo finish
+echo upload finish
